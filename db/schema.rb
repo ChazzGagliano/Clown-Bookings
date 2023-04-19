9489c20fc023ea46c9ddcca9f9cfe4eb28e17667
Chazz Gagliano <cgags17285@gmail.com>
2023-04-19 15:14:48 -0400

    add clown model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_190737) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_191228) do
+  create_table "clowns", force: :cascade do |t|
+    t.string "name"
+    t.string "speciality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
