89080d82afd47a2000b859d05635d552b15a3ada
Chazz Gagliano <cgags17285@gmail.com>
2023-04-19 16:27:48 -0400

    include associations

--- app/models/clown.rb
+++ app/models/clown.rb
@@ -1,2 +1,4 @@
 class Clown < ApplicationRecord
+  has_many :booking
+  has_many :users, through: :booking
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :booking
+  has_many :clowns, through: :booking
 end
