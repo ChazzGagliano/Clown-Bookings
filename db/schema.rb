d0f93577bd4eceff8447d3dad54ab8be8dd62efa
Chazz Gagliano <cgags17285@gmail.com>
2023-04-19 15:18:54 -0400

    add booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_191228) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_191606) do
+  create_table "bookings", force: :cascade do |t|
+    t.integer "clown_id"
+    t.integer "user_id"
+    t.integer "cost"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clowns", force: :cascade do |t|
     t.string "name"
     t.string "speciality"
