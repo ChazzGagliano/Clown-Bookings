a75237fef1857226418457b210ae99f22110db88
Chazz Gagliano <cgags17285@gmail.com>
2023-04-22 02:37:44 -0400

    add signup action(create)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :booking
   has_many :clowns, through: :booking
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_191606) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_063134) do
   create_table "bookings", force: :cascade do |t|
     t.integer "clown_id"
     t.integer "user_id"
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_191606) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "password_digest"
   end
 
 end
